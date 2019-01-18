48db052e6a798d1eb06cd393ab9f4c44b61d383b
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-01-17 19:23:15 -0800

    map out greetMe function with pseudocode

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 function greetMe() {
     console.log('greetMe function called');
 
+    // get the inputs by id
+    
+    //get values out of inputs
+
+    //make the greeting
+
+    //display greeting
+
+    //remmove prompt
 }
+
+//TODO: validate anme inputs
+//TODO: clear inputs after greet?
+//TODO: other features that I want to add
+
 
